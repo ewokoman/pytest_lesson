63d2f6eae7446395828cd1ffa6261dbb5b3001b5
Your Name <you@example.com>
2023-02-06 14:30:50 +0300

    update test_items

--- test_items.py
+++ test_items.py
@@ -10,10 +10,9 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_find_button_buy(browser):
     browser.implicitly_wait(5)
     browser.get(link)
-    time.sleep(10)
+    time.sleep(30)
     button_buy = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
 
     assert None != button_buy, \
         f"Button buy not found"  
 
-    time.sleep(10)
